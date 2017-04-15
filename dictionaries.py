14ea081c02c8d726c8e055da7fe8a6a5cb0c3bea
Bernard <bernard@sanctum.com>
2017-04-15 15:19:19 +0100

    more

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#"we create indexes for dictionaries caleed keys and values"
+#Order of the items is not persistent or in other words not maintanied
+#The values list can be a string list or touples
+dict= {"name": "jhon", "age": "25", "place": ["calicut","london","dublin"]}
+print (dict["name"])
+print (dict["place"][2])
