e781a4553f819528edf124d9d249c89808730d2b
Bernard <bernard@sanctum.com>
2017-04-17 09:45:54 +0100

    more

---
+++ fileread.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+files = open("test.txt",'r')
+content =files.readlines()
+cleanlist=[]
+for i in content :
+    cleanlist.append(i.rstrip())
+files.close()
+for items in cleanlist :
+    print (items)
+
+# the same can be achieved by creating a clean list on the fly using list comprehension
+
+content=[i.rstrip() for i in content]
